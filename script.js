30396c8d92ada31115926ec803c9fbe9545aa0a6
Sahil Kapoor <sk2377@cornell.edu>
2021-09-09 10:15:09 -0400

    Finished redisgn

---
+++ script.js
@@ -0,0 +1,27 @@
+function showEarly() {
+  document.getElementById('early-text').style.display = "block";
+  document.getElementById('college-text').style.display = "none";
+  document.getElementById('work-text').style.display = "none";
+  document.getElementById('career-text').style.display = "none";
+}
+
+function showCollege() {
+  document.getElementById('college-text').style.display = "block";
+  document.getElementById('early-text').style.display = "none";
+  document.getElementById('work-text').style.display = "none";
+  document.getElementById('career-text').style.display = "none";
+}
+
+function showWork() {
+  document.getElementById('work-text').style.display = "block";
+  document.getElementById('early-text').style.display = "none";
+  document.getElementById('college-text').style.display = "none";
+  document.getElementById('career-text').style.display = "none";
+}
+
+function showCareer() {
+  document.getElementById('career-text').style.display = "block";
+  document.getElementById('early-text').style.display = "none";
+  document.getElementById('work-text').style.display = "none";
+  document.getElementById('college-text').style.display = "none";
+}
